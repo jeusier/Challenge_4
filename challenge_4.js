bdaec616a244d1da91db122642d8bb9f641bec75
jahsan87 <jahsan87@gmail.com>
2014-02-28 10:00:59 -1000

    completed both tests

--- challenge_4.js
+++ challenge_4.js
@@ -12,6 +12,31 @@ exports.largestPalindromeProduct = function(multiplicands, digits){
   var factor_1 = 0;
   var palindromeNumber = 0;
   // do your work here
+  var minDigit = Math.pow(10, (digits-1));
+  var maxDigit = Math.pow(10, digits) - 1;
+  //console.log("min: "+minDigit);
+  //console.log("max: "+maxDigit);
+
+  for (var i = maxDigit; i >= minDigit; i--) {
+    for (var j = maxDigit; j >= minDigit; j--) {
+        product = i * j;
+        reverseProduct = parseInt(product.toString().split('').reverse().join(''));
+       // console.log("product: "+product);
+        //console.log("reverse: "+reverseProduct);
+        if(product === reverseProduct) {
+            if(palindromeNumber < product) {
+                palindromeNumber = product;
+                factor_0 = i;
+                factor_1 = j;
+                console.log("num1: "+i);
+                console.log("num2: "+j);
+                console.log(palindromeNumber);
+            }
+        }
+
+    }
+  }
+
 
 
   
